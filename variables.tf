653d81b272bb0b898b6a2e8e30951ba519de445b
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-20 19:42:30 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  description = "aws region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the subnet"
+  type        = string
+}
+
+variable "ami" {
+  description = "The AMI to use for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type to use"
+  type        = string
+  default     = "t2.micro"
+}
